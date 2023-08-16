e4ba8b99079d28af46dc6cafd6505d3571f7b01c
Shivang1001 <shivonweb2002@gmail.com>
2023-08-17 03:01:07 +0530

    post and get updated

--- api/routes/products.js
+++ api/routes/products.js
@@ -11,34 +11,42 @@ router.get('/', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
   const product = new Product({
-    _id: new mongoose.Types.objectId(),
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price
   });
-  
+
   product.save().then(result => {
     console.log(result);
+    res.status(201).json({
+      message: 'Handling POST requests to /products',
+      createdProduct: result
+    });
   })
-  .catch(err => console.log(err));
-
-  res.status(201).json({
-    message: 'Handling POST requests to /products',
-    createdProduct: product
-  });
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({error: err});
+});
 });
 
 router.get('/:productId', (req, res, next) => {
   const id = req.params.productId;
-  if(id === 'special'){
-    res.status(200).json({
-      message: 'You discovered the special ID',
-      id: id
-    });
-  } else {
-    res.status(200).json({
-      message: 'You passed an ID'
-    });
-  }
+  Product.findById(id)
+  .exec()
+  .then(doc => {
+    console.log("From database",doc);
+    if(doc)
+    {
+        res.status(200).json(doc);
+    }
+    else{
+      res.status(404).json({message: 'No valid entry found for ID'});
+    }  
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({error: err});
+  });
 });
 
 router.patch('/:productId', (req, res, next) => {
