549fc0abef0ddb2aff27a798b6ac86646822683d
Shivang1001 <shivonweb2002@gmail.com>
2023-09-27 00:12:24 +0530

    user signup

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders')
+const userRoutes = require('./api/routes/users')
 dotenv.config()
 
 mongoose.connect(`mongodb+srv://goelshivang123:${process.env.MONGO_ATLAS_PW}@cluster0.njp5xw3.mongodb.net/?retryWrites=true&w=majority`
@@ -35,6 +36,7 @@ app.use((req,res,next) =>{
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/users', userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error('Not found');
